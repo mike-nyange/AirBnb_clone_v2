e648adb6a4dccbc3bae36d710f8e943c5b189f3a
mike-nyange <micknyange@gmail.com>
2022-12-07 23:32:35 +0300

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+'''
+    Implementation of the Amenity class
+'''
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    class Amenity(BaseModel, Base):
-        """ State Class """
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
-        __tablename__ = 'amenities'
+
+class Amenity(BaseModel, Base):
+    '''
+        Implementation for the Amenities.
+    '''
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        from models.place import place_amenity
         name = Column(String(128), nullable=False)
-
-else:
-    class Amenity(BaseModel):
-        """ State Class """
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       back_populates="amenities")
+    else:
         name = ""
